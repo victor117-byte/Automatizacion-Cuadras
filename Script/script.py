221702f3a12890ad752c333952ad4154f69a1275
victor117-byte <victor117.berrios@gmail.com>
2023-03-31 11:34:05 -0600

    Mineria de texto a pdf's terminada v1 31/03/23

--- Script/script.py
+++ Script/script.py
@@ -6,7 +6,7 @@ import json
 import re
 
 # root = r"C:\Users\Victor\Documents\Cursos\Python\Automatizacion\PDF"
-root = r"../PDF/"
+root = r"C:\Users\Victor\Documents\Cursos\Python\Automatizacion\PDF"
 path = os.path.join(root, "PDF")
 
 # Funcion que obtiene los archivos con su ruta y se encarga de clasificarlos en arreglos
@@ -80,7 +80,7 @@ def exportDataAcusePresentacion(Archivos):
             # print("pdf: ", i+1, "pagina: ",pag+1)
             texto += reader._get_page(pag).extractText()
 
-        print("PDF: ", i+1)
+        # print("PDF: ", i+1)
         txt = re.sub("\n", " ", texto)
         # print(txt)  # Muestra el texto en de todo el PDF
 
@@ -88,29 +88,30 @@ def exportDataAcusePresentacion(Archivos):
         # DATOS GENERALES
 
         # rfc
-        rfc = re.search("RFC:.(.*?\s)", txt)[0]
+        rfc = re.search("RFC:.(.*?\s)", txt).group(1)
         # Fecha
         try:
             Fecha_y_hora_presentacion = re.search(
                 "Fecha y hora de presentaci.n:.{11}", txt)[0]
+            Fecha_y_hora_presentacion = re.split(':', Fecha_y_hora_presentacion)[1]
         except:
             Fecha_y_hora_presentacion = "NA"
 
         # numero de operacion
         try:
             Num_de_Operacion = re.search(
-                "N.mero de operaci.n:.(\d*?\s)", txt)[0]
+                "N.mero de operaci.n:.(\d*?\s)", txt).group(1)
         except:
             Num_de_Operacion = "NA"
         # periodo de la declaracion
         try:
             Periodo_de_declaracion = re.search(
-                "Per.odo de la declaraci.n:\s.(.+?\s)", txt)[0]
+                "Per.odo de la declaraci.n:\s(.+?\s)", txt).group(1)
         except:
             Periodo_de_declaracion = "NA"
         # Ejercicio
         try:
-            Ejercicio = re.search("Ejercicio:\s.(.+?\s)", txt)[0]
+            Ejercicio = re.search("Ejercicio:\s(.+?\s)", txt).group(1)
         except:
             Ejercicio = "NA"
         # Denominación o razón social:
@@ -122,14 +123,14 @@ def exportDataAcusePresentacion(Archivos):
 
         # SELECCION LINEA DE CAPTURA
         try:
-            total_a_pagar = re.search("total a pagar:(.+?\s)", txt)[0]
-            Vigente_hasta = re.search("Vigente hasta:(.+?\s)", txt)[0]
+            total_a_pagar = re.search("total a pagar:(.+?\s)", txt).group(1)
+            Vigente_hasta = re.search("Vigente hasta:(.+?\s)", txt).group(1)
             Linea_de_Captura = "Linea de Captura : " + \
                 re.search("Línea de Captura:.(.*?)Importe", txt).group(1)
         except:
-            total_a_pagar = "total a pagar: NA "
-            Vigente_hasta = "Vigente hasta: NA "
-            Linea_de_Captura = "Linea de Captura : NA "
+            total_a_pagar = "NA"
+            Vigente_hasta = "NA"
+            Linea_de_Captura = "NA "
         # SALDO A FAVOR
         try:
             Impuesto_a_favor = re.search(
@@ -143,6 +144,7 @@ def exportDataAcusePresentacion(Archivos):
         
 
         data_fila = np.array([
+            "Acuse Recepcion",
             rfc,
             Fecha_y_hora_presentacion,
             Num_de_Operacion,
@@ -158,6 +160,99 @@ def exportDataAcusePresentacion(Archivos):
 
     return array_docs
 
+def exportDataAcuseRecepcion(Archivos):
+    array_docs = []
+    for i in range(len(Archivos)):
+
+        pdf = open(Archivos[i], 'rb')
+        # print(Archivos[i])
+        reader = PyPDF2.PdfReader(pdf)
+        page = reader.pages
+        texto = ''
+
+        for pag in range(len(page)):
+            # print("pdf: ", i+1, "pagina: ",pag+1)
+            texto += reader._get_page(pag).extractText()
+
+        # print("PDF: ", i+1)
+        txt = re.sub("\n", " ", texto)
+        # print(txt)  # Muestra el texto en de todo el PDF
+
+        # Text Mining
+        # DATOS GENERALES
+
+        # rfc
+        rfc = re.search("RFC:.(.*?\s)", txt).group(1)
+        # Fecha
+        try:
+            Fecha_y_hora_presentacion = re.search(
+                "Fecha y hora de presentaci.n:.{11}", txt)[0]
+            Fecha_y_hora_presentacion = re.split(':', Fecha_y_hora_presentacion)[1]
+        except:
+            Fecha_y_hora_presentacion = "NA"
+
+        # numero de operacion
+        try:
+            Num_de_Operacion = re.search(
+                "N.mero de operaci.n:.(\d*?\s)", txt).group(1)
+        except:
+            Num_de_Operacion = "NA"
+        # periodo de la declaracion
+        try:
+            Periodo_de_declaracion = re.search(
+                "Per.odo de la declaraci.n:\s(.+?\s)", txt).group(1)
+        except:
+            Periodo_de_declaracion = "NA"
+        # Ejercicio
+        try:
+            Ejercicio = re.search("Ejercicio:\s(.+?\s)", txt).group(1)
+        except:
+            Ejercicio = "NA"
+        # Denominación o razón social:
+
+        try:
+            RazonSocial = re.search("Hoja\d\sde\s\d.(.*?)Tipo", txt).group(1)
+        except:
+            RazonSocial = 'NA'
+
+        # SELECCION LINEA DE CAPTURA
+        try:
+            total_a_pagar = re.search("total a pagar:(.+?\s)", txt).group(1)
+            Vigente_hasta = re.search("Vigente hasta:(.+?\s)", txt).group(1)
+            Linea_de_Captura = "Linea de Captura : " + \
+                re.search("Línea de Captura:.(.*?)Importe", txt).group(1)
+        except:
+            total_a_pagar = "NA"
+            Vigente_hasta = "NA"
+            Linea_de_Captura = "NA "
+        # SALDO A FAVOR
+        try:
+            Impuesto_a_favor = re.search(
+                "Impuesto.a.favor:.(.*?)ACUSE", txt).group(1)
+            Impuesto_a_favor = re.split(' ', Impuesto_a_favor)[0]
+            # print(Impuesto_a_favor)
+        except:
+            Impuesto_a_favor = 'NA'
+
+        # data_fila = [rfc, Fecha_y_hora_presentacion, Num_de_Operacion, Periodo_de_declaracion, Ejercicio,  total_a_pagar, Vigente_hasta, Linea_de_Captura, Archivos[i]]
+        
+
+        data_fila = np.array([
+            "Acuse Recepcion",
+            rfc,
+            Fecha_y_hora_presentacion,
+            Num_de_Operacion,
+            Periodo_de_declaracion,
+            Ejercicio,
+            total_a_pagar,
+            Vigente_hasta,
+            Linea_de_Captura,
+            RazonSocial,
+            Impuesto_a_favor,
+            Archivos[i]])
+        array_docs.append(data_fila)
+
+    return array_docs
 
 def exportDataDiot(Archivos):
     array_docs = []
@@ -173,7 +268,7 @@ def exportDataDiot(Archivos):
             # print("pdf: ", i+1, "pagina: ",pag+1)
             texto += reader._get_page(pag).extractText()
 
-        print("PDF: ", i+1)
+        # print("PDF: ", i+1)
         txt = re.sub("\n", " ", texto)
         # print(txt) #Muestra el texto en de todo el PDF
 
@@ -183,12 +278,14 @@ def exportDataDiot(Archivos):
         # Usuario
         try:
             usuario = re.search("Usuario:.(.+?\s)", txt)[0]
+            usuario = re.split(':', usuario)[1]
         except:
             usuario = "NA"
 
         # Archivo
         try:
             Archivo_Recibido = re.search("Archivo Recibido:.(.+?\s)", txt)[0]
+            Archivo_Recibido = re.split(":_", Archivo_Recibido)[1]
         except:
             Archivo_Recibido = "NA"
 
@@ -200,22 +297,26 @@ def exportDataDiot(Archivos):
         # Fecha de recepcion
         try:
             Fecha_Recepcion = re.search("Fecha de Recepci.n:.(.+?\s)", txt)[0]
+            Fecha_Recepcion = re.split(':', Fecha_Recepcion)[1]
         except:
             Fecha_Recepcion = "NA"
         # Hora de recepcion
         try:
             Hora_Recepcion = re.search("Hora de Recepci.n:.(.+?\s)", txt)[0]
+            Hora_Recepcion = re.split('Recepci.n:', Hora_Recepcion)[1]
         except:
             Hora_Recepcion = "NA"
         # Folio
         try:
             Folio = re.search("Folio de Recepci.n:.(.+?\s)", txt)[0]
+            Folio = re.split(':', Folio)[1]
         except:
             Folio = "NA"
 
         
 
         data_fila = np.array([
+            "Diot",
             usuario,
             Archivo_Recibido,
             tamanio,
@@ -227,7 +328,6 @@ def exportDataDiot(Archivos):
         array_docs.append(data_fila)
 
     return array_docs
-
 
 def exportDataConstancia(Archivos):
     array_docs = []
@@ -278,12 +378,12 @@ def exportDataConstancia(Archivos):
                 Regimen_Nombre = "NA"
         # PRIMER APELLIDO
         try:
-            Primer_Apellido = re.search("PrimerApellido:(.+?\s)", txt)[0]
+            Primer_Apellido = re.search("PrimerApellido:(.+?\s)", txt).group(1)
         except:
             Primer_Apellido = 'NA'
         # SEGUNDO APELLIDO
         try:
-            Segundo_Apellido = re.search("Segundo.Apellido:(.+?\s)", txt)[0]
+            Segundo_Apellido = re.search("Segundo.Apellido:(.+?\s)", txt).group(1)
         except:
             Segundo_Apellido = 'NA'
         # NOMBRE COMERICAL
@@ -305,7 +405,7 @@ def exportDataConstancia(Archivos):
             Fecha_Operaciones = 'NA'
         # ESTATUS
         try:
-            Estatus = re.search("Estatusenelpadr.n:(.+?\s)", txt)[0]
+            Estatus = re.search("Estatusenelpadr.n:(.+?\s)", txt).group(1)
         except:
             Estatus = 'NA'
         # REGIMENES
@@ -318,6 +418,7 @@ def exportDataConstancia(Archivos):
         
 
         data_fila = np.array([
+            "Constancia",
             rfc,
             Razon_Social_Nombre,
             Regimen_Nombre,
@@ -334,7 +435,6 @@ def exportDataConstancia(Archivos):
 
     return array_docs
 
-
 def exportDataOpinionCumplimiento(Archivos):
     array_docs = []
     for i in range(len(Archivos)):
@@ -349,9 +449,9 @@ def exportDataOpinionCumplimiento(Archivos):
             # print("pdf: ", i+1, "pagina: ",pag+1)
             texto += reader._get_page(pag).extractText()
 
-        print("PDF: ", i+1)
+        # print("PDF: ", i+1)
         txt = re.sub("\n", " ", texto)
-        print(txt)  # Muestra el texto en de todo el PDF
+        # print(txt)  # Muestra el texto en de todo el PDF
 
         # Text Mining
         # DATOS GENERALES
@@ -359,19 +459,20 @@ def exportDataOpinionCumplimiento(Archivos):
         # Folio
         try:
             folio = re.search('Folio.(.+?\s)(.+?\s)Respuesta', txt).group(2)
-            print(folio)
+            # print(f'Folio: {folio} Tiene una longitud de: ', len(folio))
         except:
             folio = 'NA'
         # Clave RFC
         try:
             RFC = re.search('Folio.(.+?\s)(.+?\s)Respuesta', txt).group(1)
-            print(RFC)
+            # print(f'RFC: {RFC} Tiene una longitud de: ', len(RFC))
         except:
             RFC = 'NA'
 
         data_fila = np.array([
-            folio,
+            'Opinion cumplimiento',
             RFC,
+            folio,
             Archivos[i]
         ])
         array_docs.append(data_fila)
@@ -380,22 +481,33 @@ def exportDataOpinionCumplimiento(Archivos):
 
 
 # # ACUSE RECEPCION
-column_names = ["rfc","Fecha_y_hora_presentacion","Num_de_Operacion","Periodo_de_declaracion","Ejercicio","total_a_pagar","Vigente_hasta","Linea_de_Captura","RazonSocial","Impuesto_a_favor","Path"]
-df_Acuse_Recepcion = pd.DataFrame(exportDataAcusePresentacion(Archivos_Acuse_Recepcion), columns=column_names)
+column_names = ["PDF","RFC","Fecha_y_hora_presentacion","Num_de_Operacion","Periodo_de_declaracion","Ejercicio","total_a_pagar","Vigente_hasta","Linea_de_Captura","Razon_Social","Impuesto_a_favor","Path"]
+df_Acuse_Recepcion = pd.DataFrame(exportDataAcuseRecepcion(Archivos_Acuse_Recepcion), columns=column_names)
+
+# # ACUSE Presentacion
+column_names = ["PDF","RFC","Fecha_y_hora_presentacion","Num_de_Operacion","Periodo_de_declaracion","Ejercicio","total_a_pagar","Vigente_hasta","Linea_de_Captura","Razon_Social","Impuesto_a_favor","Path"]
+df_Acuse_Presentacion = pd.DataFrame(exportDataAcusePresentacion(Archivos_Acuse_Presentacion), columns=column_names)
 
 # # DIOT
-column_names = ["usuario", "Archivo_Recibido", "tamanio","Fecha_Recepcion", "Hora_Recepcion", "Folio", "Path"]
+column_names = ["PDF","Usuario", "Archivo_Recibido", "tamanio","Fecha_Recepcion", "Hora_Recepcion", "Folio", "Path"]
 df_Diot = pd.DataFrame(exportDataDiot(Archivos_Diot), columns=column_names)
 
 
 # CONTANCIA
-column_names=["RFC", "Razon Social", "CURP", "Primer Apellido", "Segundo Apellido", "Nombre Comercial", "Fecha de Operacion", "Estatus", "Regimenes", "Path"]
+column_names=["PDF","RFC", "Razon_Social", "CURP", "Primer_Apellido", "Segundo_Apellido", "Nombre_Comercial", "Fecha_Operacion", "Estatus", "Regimenes", "Path"]
 df_Constancia = pd.DataFrame(exportDataConstancia(Archivos_Constancia), columns=column_names)
 
 
 
-# Prueba
-# datos_Opinion = exportDataOpinionCumplimiento(Archivos_Opinion_Cumplimiento)
+# Opinion de cumplimiento
+column_names=["PDF","RFC", "Folio", "Path"]
+df_Opinion_Cumplimiento =  pd.DataFrame(exportDataOpinionCumplimiento(Archivos_Opinion_Cumplimiento), columns=column_names)
 
-# df = pd.DataFrame(datos_Opinion)
-# df
+Export = pd.concat([df_Acuse_Recepcion,
+                    df_Acuse_Presentacion,
+                    df_Diot,
+                    df_Constancia,
+                    df_Opinion_Cumplimiento
+                    ])
+
+# Export
